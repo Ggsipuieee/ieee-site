bb066a26d285b046fb35509aae72b21b747e1b04
Nilakshhh <97288540+Nilakshhh@users.noreply.github.com>
2022-09-14 18:44:26 +0530

    x

--- src/components/Sectionourteam.jsx
+++ src/components/Sectionourteam.jsx
@@ -6,7 +6,7 @@ function Sectionourteam() {
              <Heading text="Our Team" class="aboutourteam"/>
         <div>
         <div className="teacher-card">
-          <Card src="./images/ashish.jpeg" text="Ashish Payal" content="He is an excellent academician, holding a PhD as well as MTech in Computer Science along with MBA in HR and is currently serving as an Assistant Professor in USICT. He has over a decade of teaching experience with expertise in Computer Networks, supplemented by a myriad of research papers. He has mentored and guided hundreds of students and has been a very active part of IEEE USICT as our Branch Counselor."/>
+          <Card src="../images/ashish.jpeg" text="Ashish Payal" content="He is an excellent academician, holding a PhD as well as MTech in Computer Science along with MBA in HR and is currently serving as an Assistant Professor in USICT. He has over a decade of teaching experience with expertise in Computer Networks, supplemented by a myriad of research papers. He has mentored and guided hundreds of students and has been a very active part of IEEE USICT as our Branch Counselor."/>
           <Card src="./images/kamaldeep.jpg" text="Kamaldeep Kaur" content="has a PhD and MTech in CSE and expertise in Computer Architecture, Microprocessors and Digital Electronics. She is currently an Assistant Professor in USICT. She has always been vocal in support of Women in Engineering and Technical Fields and is always open to assist and help students who approach her for guidance. She presides over our WIE subchapter as the WIE Branch Counselor for USICT."/>
         </div>
         <Heading text="Our Executive Comittee" class="aboutourteam"/>
