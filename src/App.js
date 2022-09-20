27bb3d85e9484a0fd0150915a7cd96f38c5d75d3
Nilakshhh <97288540+Nilakshhh@users.noreply.github.com>
2022-09-20 19:13:57 +0530

    s

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import {
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Header from './components/Header';
-import Contact from './pages/Contact.jsx';
 import Footer from './components/Footer';
 import Events from './pages/Events';
-import Aarambh from './pages/Aarambh';
 
 function App() {
 
@@ -20,8 +18,6 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/events' element={<Events />} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/aarambh' element={<Aarambh />} />
       </Routes>
     <Footer />
     </Router>
