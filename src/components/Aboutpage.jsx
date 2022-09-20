e69624beb278738e0d1b54cc723eb31770af030d
Nilakshhh <97288540+Nilakshhh@users.noreply.github.com>
2022-09-20 21:36:29 +0530

    s

--- src/components/Aboutpage.jsx
+++ src/components/Aboutpage.jsx
@@ -3,16 +3,14 @@ import React from "react";
 function Aboutpage(){
 return(
     <div>
-
-
 <div class="about-us-intro">
       <h1 class="heading-main"><strong>a</strong>bout us</h1>
     </div>
 
     <section class="hero-section">
-      <h1 class="hero-section-heading">advancing technology for humanity.</h1>
+      <h1 class="hero-section-heading">Advancing technology for humanity.</h1>
       <img
-        src="images/flowers-276014__340.jpeg"
+        src="./images/globe.jpg"
         alt=""
         class="hero_section-image"
       />
@@ -22,143 +20,77 @@ return(
       <div class="our-mission_content">
         <h1 class="content-heading">our mission</h1>
         <p class="content-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi iste
-          magnam ullam placeat, consectetur aspernatur assumenda qui odit
-          accusamus iure totam consequuntur velit voluptatibus dolore asperiores
-          atque maiores eius minus in eveniet, impedit dolores pariatur nostrum
-          quam? Molestiae laborum temporibus at tenetur, asperiores eum eaque,
-          corrupti facilis modi labore cumque repudiandae maiores non maxime
-          nisi aliquid in ea quod! Soluta!
+          IEEE GGSIPU aims to organise events, connect with more and more students and be a portal for learning, looking at new ways and understanding ever-growing technology even better.
         </p>
       </div>
       <div class="our-mission-images">
-        <img src="images/flowers-276014__340.jpeg" alt="" class="ieee-lead" />
+        <img src="./images/lightbulb.jpg" alt="" class="ieee-lead" />
       </div>
     </section>
 
     <section class="ieee-information">
       <div class="main-card">
-        <h1 class="main-card_heading card-heading">IEEE INFORMATION</h1>
+        <h1 class="main-card_heading card-heading">Why Join IEEE?</h1>
         <p class="main-card_text card-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe modi
-          natus impedit animi ullam. Quia vel dolorem ipsam exercitationem
-          sapiente. Deserunt placeat reiciendis hic ipsam velit sit assumenda ad
-          deleniti.
+        IEEE membership offers access to technical innovation, cutting-edge information, networking opportunities, and exclusive member benefits. Members support IEEE's mission to advance technology for humanity and the profession, while memberships build a platform to introduce careers in technology to students around the world.
         </p>
-        <a href="#" class="card-link">Read more</a>
+        <a href="https://www.ieee.org/membership/benefits/index.html" class="card-link">Read more</a>
       </div>
 
       <div class="information-card">
         <div class="information-card_header">
-          <img src="images/smiley.png" alt="" class="iee-icons" />
-          <h1 class="card-heading">IEEE INFORMATION</h1>
+        <i class="fa-solid fa-users-medical ieee-icons"></i>
+          <h1 class="card-heading">Networking</h1>
         </div>
         <p class="card-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe modi
-          natus impedit animi ullam. Quia vel dolorem ipsam exercitationem
-          sapiente. Deserunt placeat reiciendis hic ipsam velit sit assumenda ad
-          deleniti.
+        Professional networking is a necessary skill for a lifetime of career success. Network with an unparalleled, global community of professionals.
         </p>
-        <a href="#" class="card-link info-card-link">Read more</a>
+        
       </div>
 
       <div class="information-card">
         <div class="information-card_header">
-          <img src="images/smiley.png" alt="" class="iee-icons" />
-          <h1 class="card-heading">IEEE INFORMATION</h1>
+        <i class="fa-duotone fa-laptop-mobile ieee-icons"></i>
+          <h1 class="card-heading">Stay Updated with Technology</h1>
         </div>
         <p class="card-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe modi
-          natus impedit animi ullam. Quia vel dolorem ipsam exercitationem
-          sapiente. Deserunt placeat reiciendis hic ipsam velit sit assumenda ad
-          deleniti.
+        Technology professionals and engineers will always be continually challenged to keep abreast of new and changing technology. In addition to your studies, IEEE has the means for you to remain technically current through competitions and award-winning publications.
         </p>
-        <a href="#" class="card-link info-card-link">Read more</a>
+        
       </div>
 
       <div class="information-card">
         <div class="information-card_header">
-          <img src="images/smiley.png" alt="" class="iee-icons" />
-          <h1 class="card-heading">IEEE INFORMATION</h1>
+        <i class="fa-solid fa-book-bookmark ieee-icons"></i>
+          <h1 class="card-heading">Career Resources</h1>
         </div>
         <p class="card-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe modi
-          natus impedit animi ullam. Quia vel dolorem ipsam exercitationem
-          sapiente. Deserunt placeat reiciendis hic ipsam velit sit assumenda ad
-          deleniti.
+        Being a student is the first job of your professional career. Let IEEE help with valuable scholarship, and award program opportunities.
         </p>
-        <a href="#" class="card-link info-card-link">Read more</a>
+        
       </div>
 
       <div class="information-card">
         <div class="information-card_header">
-          <img src="images/smiley.png" alt="" class="iee-icons" />
-          <h1 class="card-heading">IEEE INFORMATION</h1>
+        <i class="fa-sharp fa-solid fa-building-columns ieee-icons"></i>
+          <h1 class="card-heading">Continuing Education</h1>
         </div>
         <p class="card-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe modi
-          natus impedit animi ullam. Quia vel dolorem ipsam exercitationem
-          sapiente. Deserunt placeat reiciendis hic ipsam velit sit assumenda ad
-          deleniti.
+        IEEE student members have access to numerous continuing education opportunities at discounted rates. Keep growing with educational opportunities available for IEEE members.
         </p>
-        <a href="#" class="card-link info-card-link">Read more</a>
+    
       </div>
 
       <div class="information-card">
         <div class="information-card_header">
-          <img src="images/smiley.png" alt="" class="iee-icons" />
-          <h1 class="card-heading">IEEE INFORMATION</h1>
+        <i class="fa-light fa-badge-percent ieee-icons"></i>
+          <h1 class="card-heading">Discounts</h1>
         </div>
         <p class="card-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe modi
-          natus impedit animi ullam. Quia vel dolorem ipsam exercitationem
-          sapiente. Deserunt placeat reiciendis hic ipsam velit sit assumenda ad
-          deleniti.
+        Students reap the same benefits as professional members, such as discounts on many technical resources, and reduced registration fees for conferences, plus student discounts on Society memberships, publications and more. In addition, student members have access to a variety of affinity programs that offer savings.
         </p>
-        <a href="#" class="card-link info-card-link">Read more</a>
-      </div>
-
-      <div class="information-card">
-        <div class="information-card_header">
-          <img src="images/smiley.png" alt="" class="iee-icons" />
-          <h1 class="card-heading">IEEE INFORMATION</h1>
-        </div>
-        <p class="card-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe modi
-          natus impedit animi ullam. Quia vel dolorem ipsam exercitationem
-          sapiente. Deserunt placeat reiciendis hic ipsam velit sit assumenda ad
-          deleniti.
-        </p>
-        <a href="#" class="card-link info-card-link">Read more</a>
-      </div>
-      <div class="information-card">
-        <div class="information-card_header">
-          <img src="images/smiley.png" alt="" class="iee-icons" />
-          <h1 class="card-heading">IEEE INFORMATION</h1>
-        </div>
-        <p class="card-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe modi
-          natus impedit animi ullam. Quia vel dolorem ipsam exercitationem
-          sapiente. Deserunt placeat reiciendis hic ipsam velit sit assumenda ad
-          deleniti.
-        </p>
-        <a href="#" class="card-link info-card-link">Read more</a>
-      </div>
-      <div class="information-card">
-        <div class="information-card_header">
-          <img src="images/smiley.png" alt="" class="iee-icons" />
-          <h1 class="card-heading">IEEE INFORMATION</h1>
-        </div>
-        <p class="card-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe modi
-          natus impedit animi ullam. Quia vel dolorem ipsam exercitationem
-          sapiente. Deserunt placeat reiciendis hic ipsam velit sit assumenda ad
-          deleniti.
-        </p>
-        <a href="#" class="card-link info-card-link">Read more</a>
       </div>
     </section>
-
     </div>
 )
 
